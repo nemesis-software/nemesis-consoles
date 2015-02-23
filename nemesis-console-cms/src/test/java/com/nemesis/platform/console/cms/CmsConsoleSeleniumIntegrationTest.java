c3d8924a4ce47129828d1820c051150de99fefb2
Petar Tahchiev <paranoiabla@gmail.com>
2015-02-23 23:08:55 +0200

    adding tests for cms console

---
+++ nemesis-console-cms/src/test/java/com/nemesis/platform/console/cms/CmsConsoleSeleniumIntegrationTest.java
@@ -0,0 +1,114 @@
+/*
+ * nemesis Platform - NExt-generation Multichannel E-commerce SYStem
+ *
+ * Copyright (c) 2010 - 2014 nemesis
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of nemesis
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with nemesis.
+ */
+package com.nemesis.platform.console.cms;
+
+import com.nemesis.console.common.AbstractCommonConsoleSeleniumInterationTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A selenium test-case for the cms console.
+ *
+ * @author Petar Tahchiev
+ * @since 0.6
+ */
+public class CmsConsoleSeleniumIntegrationTest extends AbstractCommonConsoleSeleniumInterationTest {
+
+    public static RemoteWebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        driver = new FirefoxDriver();
+
+        driver.get("http://localhost:8080/cms");
+
+        assertEquals("Login Page", driver.getTitle());
+
+        driver.findElement(By.cssSelector("input[name=username]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("nimda");
+        driver.findElement(By.cssSelector("input[name=submit]")).click();
+
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cms console | nemesis");
+            }
+        });
+
+        assertEquals("cms console | nemesis", driver.getTitle().toLowerCase());
+
+        waitForDom();
+        waitForLoad();
+    }
+
+    protected static void waitForDom() {
+        driver.executeScript("Ext.onReady(function () {});");
+    }
+
+    protected static void waitForLoad() {
+        ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(pageLoadCondition);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.findElementById("app-header-logout").click();
+        Thread.sleep(500);
+        assertEquals("Login Page", driver.getTitle());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testHeaderLinkReloadsPage() {
+        driver.findElement(By.cssSelector("a#app-header-title")).click();
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().startsWith("CMS Console | Nemesis");
+            }
+        });
+    }
+
+    @Test
+    public void testChangeLocale() {
+        //Change locale
+        driver.executeScript(
+                        "var c = Ext.getCmp('app-header-language-selector'); c.setValue({'isoCode':'bg'}); c.fireEvent('select', c, {data : {'isoCode':'bg'}});");
+
+        // Wait for the page to load, timeout after 5 seconds
+        (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().startsWith("CMS Console | Nemesis");
+            }
+        });
+
+        assertEquals("Изход", driver.findElementById("app-header-logout").getText());
+
+    }
+
+}
