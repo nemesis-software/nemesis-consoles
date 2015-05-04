b7f5600c44496df9fef1e1ee240d57c719fbcf15
Miroslav Nenov <m.nenov92@gmail.com>
2015-05-04 12:14:00 +0300

    #25 Memory usage portlet fixed

--- nemesis-console-admin/src/main/webapp/resources/app/view/portlet/MemoryUsagePortlet.js
+++ nemesis-console-admin/src/main/webapp/resources/app/view/portlet/MemoryUsagePortlet.js
@@ -2,6 +2,7 @@ Ext.define('AdminConsole.view.portlet.MemoryUsagePortlet', {
     xtype: 'memoryUsagePortlet',
     extend: 'Ext.panel.Panel',
     layout: 'fit',
+    height: 300,
     requires: [
         'Ext.data.JsonStore',
         'Ext.chart.theme.Base',
@@ -9,63 +10,38 @@ Ext.define('AdminConsole.view.portlet.MemoryUsagePortlet', {
         'Ext.chart.series.Line',
         'Ext.chart.axis.Numeric'
     ],
-    initComponent: function () {
+    initComponent: function() {
         var me = this;
         var chart;
-        var generateData = (function () {
-            var data = [], date = new Date(2011, 1, 1), i = 0;
-            return function () {
+        var generateData = (function() {
+            var data = [],
+                date = new Date(2011, 1, 1),
+                i = 0;
+
+            return function() {
                 data = data.slice();
 
-                Ext.Ajax.defaultHeaders = {'nemesis-username': Ext.get('username').dom.getAttribute('value'), 'nemesis-token': Ext.get('token').dom.getAttribute('value'), 'nemesis-expiryTime': Ext.get('expiryTime').dom.getAttribute('value')};
-
-                Ext.Ajax.request({ url: Ext.get('rest-base-url').dom.getAttribute('url') + 'platform/resource-usage',
+                Ext.Ajax.request({
+                    url: Ext.get('rest-base-url').dom.getAttribute('url') + 'platform/metrics',
                     method: 'GET',
-                    params: {'nemesis-username': Ext.get('username').dom.getAttribute('value'), 'nemesis-token': Ext.get('token').dom.getAttribute('value'), 'nemesis-expiryTime': Ext.get('expiryTime').dom.getAttribute('value')},
-                    success: function (responseObject) {
+                    success: function(responseObject) {
                         var result = Ext.decode(responseObject.responseText);
+                        var percentageMemoryUsage = (result['mem.free'] / result.mem) * 100;
                         data.push({
                             date: Ext.Date.add(date, Ext.Date.DAY, i++),
-                            cpu: result.percentageCPUUsage,
-                            memoryHeap: result.percentageMemoryHeapUsage
+                            //cpu: result.percentageCPUUsage,
+                            memoryHeap: percentageMemoryUsage
                         });
                     },
-                    failure: function (responseObject) {
+                    failure: function(responseObject) {
                         var error = Ext.decode(responseObject.responseText);
                         Ext.Msg.alert('Error', 'Error: ' + error);
-                    },
-                    defaultHeaders: {
-                        'custom': 'true'
                     }
-                    //headers: {'nemesis-username': Ext.get('username').dom.getAttribute('value'), 'nemesis-token' : Ext.get('token').dom.getAttribute('value'), 'nemesis-expiryTime' : Ext.get('expiryTime').dom.getAttribute('value')}
                 });
 
                 return data;
             };
         })();
-
-        var group = false,
-            groupOp = [
-                {
-                    dateFormat: 'M d',
-                    groupBy: 'year,month,day'
-                },
-                {
-                    dateFormat: 'M',
-                    groupBy: 'year,month'
-                }
-            ];
-
-        function regroup() {
-            group = !group;
-            var axis = chart.axes[1],
-                selectedGroup = groupOp[+group];
-            axis.dateFormat = selectedGroup.dateFormat;
-            axis.groupBy = selectedGroup.groupBy;
-
-            chart.redraw();
-        }
-
 
         var store = new Ext.data.JsonStore({
             fields: ['date', 'memoryHeap', 'cpu'],
@@ -78,103 +54,93 @@ Ext.define('AdminConsole.view.portlet.MemoryUsagePortlet', {
             shadow: false,
             itemId: 'resource-usage-chart',
             store: store,
-            axes: [
-                {
-                    type: 'numeric',
-                    position: 'left',
-                    minimum: 0,
-                    maximum: 100,
-                    fields: ['memoryHeap'],
-                    title: 'Memory Usage (%)',
-                    grid: {
-                        odd: {
-                            fill: '#dedede',
-                            stroke: '#ddd',
-                            'stroke-width': 0.5
-                        }
-                    },
-                    renderer: function (v) {
-                        return v >> 0;
-                    },
-                    label: {
-                        font: '11px Arial'
+            axes: [{
+                type: 'numeric',
+                position: 'left',
+                minimum: 0,
+                maximum: 100,
+                fields: ['memoryHeap'],
+                title: 'Memory Usage (%)',
+                grid: {
+                    odd: {
+                        fill: '#dedede',
+                        stroke: '#ddd',
+                        'stroke-width': 0.5
                     }
                 },
-                {
-                    type: 'time',
-                    position: 'bottom',
-                    hidden: true,
-                    fields: 'date',
-                    dateFormat: 'M d',
-                    constrain: true,
-                    groupBy: 'year,month,day',
-                    aggregateOp: 'sum',
-                    fromDate: new Date(2011, 1, 1),
-                    toDate: new Date(2011, 1, 10)
+                renderer: function(v) {
+                    return v >> 0;
                 },
-                {
-                    type: 'numeric',
-                    position: 'right',
-                    minimum: 0,
-                    maximum: 100,
-                    fields: ['cpu'],
-                    title: 'CPU Usage (%)',
-                    renderer: function (v) {
-                        return v >> 0;
-                    },
-                    label: {
-                        font: '11px Arial'
-                    }
+                label: {
+                    font: '11px Arial'
                 }
-            ],
-            series: [
-                {
-                    type: 'line',
-                    lineWidth: 1,
-                    fill: true,
-                    showMarkers: false,
-                    axis: 'left',
-                    xField: 'date',
-                    yField: 'memoryHeap',
-                    style: {
-                        'stroke-width': 1,
-                        stroke: 'rgb(148, 174, 10)'
-                    }
-                },
-                {
-                    type: 'line',
-                    lineWidth: 1,
-                    showMarkers: false,
-                    axis: 'right',
-                    xField: 'date',
-                    yField: 'cpu',
-                    style: {
-                        'stroke-width': 1,
-                        stroke: 'rgb(17, 95, 166)'
-                    }
+            }, {
+                type: 'time',
+                position: 'bottom',
+                hidden: true,
+                fields: 'date',
+                dateFormat: 'M d',
+                constrain: true,
+                groupBy: 'year,month,day',
+                aggregateOp: 'sum',
+                fromDate: new Date(2011, 1, 1),
+                toDate: new Date(2011, 1, 10)
+                    // }, {
+                    //     type: 'numeric',
+                    //     position: 'right',
+                    //     minimum: 0,
+                    //     maximum: 100,
+                    //     fields: ['cpu'],
+                    //     title: 'CPU Usage (%)',
+                    //     renderer: function(v) {
+                    //         return v >> 0;
+                    //     },
+                    //     label: {
+                    //         font: '11px Arial'
+                    //     }
+            }],
+            series: [{
+                type: 'line',
+                lineWidth: 1,
+                fill: true,
+                showMarkers: false,
+                axis: 'left',
+                xField: 'date',
+                yField: 'memoryHeap',
+                style: {
+                    'stroke-width': 1,
+                    stroke: 'rgb(148, 174, 10)'
                 }
-            ]
-        } :
-        {
+            // }, {
+            //     type: 'line',
+            //     lineWidth: 1,
+            //     showMarkers: false,
+            //     axis: 'right',
+            //     xField: 'date',
+            //     yField: 'cpu',
+            //     style: {
+            //         'stroke-width': 1,
+            //         stroke: 'rgb(17, 95, 166)'
+            //     }
+            }]
+        } : {
             xtype: 'component',
             padding: '5',
             html: 'Advanced charting on this browser is not currently supported in this release.'
         };
-	
+
         Ext.apply(this, {
-            layout: 'fit',
-            height: 300,
             items: chart
         });
-		this.callParent(arguments);
-		chart = this.down('chart');
+        this.callParent(arguments);
+        chart = this.down('chart');
 
-        var intr = setInterval(function () {
-			timeAxis = chart.axes.get(1);
+        var intr = setInterval(function() {
+            timeAxis = chart.axes.get(1);
             var gs = generateData();
-			if (!gs.length) {
-				return;
-			}
+            if (!gs.length) {
+                return;
+            }
             var toDate = timeAxis.toDate,
                 lastDate = gs[gs.length - 1].date,
                 markerIndex = chart.markerIndex || 0;
