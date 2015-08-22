392668430d3a0c1ab62130d19928b081936bb0df
Petar Tahchiev <paranoiabla@gmail.com>
2015-08-22 16:21:25 +0300

    Comment the authentication because it is not depoyed on solarapparel.com

--- nemesis-console-common/src/main/java/com/nemesis/platform/consoles/common/storefront/security/DefaultRestAuthenticationProvider.java
+++ nemesis-console-common/src/main/java/com/nemesis/platform/consoles/common/storefront/security/DefaultRestAuthenticationProvider.java
@@ -11,41 +11,15 @@
  */
 package com.nemesis.platform.consoles.common.storefront.security;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.annotation.Resource;
-import javax.net.ssl.SSLContext;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.annotation.Resource;
 
 /**
  * @version $Id$
@@ -63,46 +37,46 @@ public class DefaultRestAuthenticationProvider implements AuthenticationProvider
         final String username = (String) authentication.getPrincipal();
         final String password = (String) authentication.getCredentials();
 
-        final UserData userData;
-        try {
-            SSLContext context = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
-            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(context, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create().register("https", csf).build();
-            HttpClientConnectionManager ccm = new PoolingHttpClientConnectionManager(registry);
+//        final UserData userData;
+        //        try {
+        //            SSLContext context = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+        //            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(context, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        //            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create().register("https", csf).build();
+        //            HttpClientConnectionManager ccm = new PoolingHttpClientConnectionManager(registry);
+        //
+        //            HttpClient httpclient = HttpClientBuilder.create().setConnectionManager(ccm).build();
+        //
+        //            /**
+        //             * It can't be POST because the CSRF is triggered.
+        //             */
+        //            HttpGet httpGet = new HttpGet(getRestBaseUrl() + "auth");
+        //
+        //            LOG.debug("Calling: " + getRestBaseUrl() + "auth");
+        //
+        //            httpGet.setHeader("X-Nemesis-Username", username);
+        //            httpGet.setHeader("X-Nemesis-Password", password);
+        //
+        //            HttpResponse response2 = httpclient.execute(httpGet);
+        //            HttpEntity entity2 = response2.getEntity();
+        //            final String response = EntityUtils.toString(entity2, Charset.defaultCharset());
+        //            LOG.info(response);
+        //            ObjectMapper mapper = new ObjectMapper();
+        //            mapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
+        //            userData = mapper.readValue(response, UserData.class);
+        //            if (userData.getToken() == null) {
+        //                throw new BadCredentialsException("Invalid username/password");
+        //            }
 
-            HttpClient httpclient = HttpClientBuilder.create().setConnectionManager(ccm).build();
+        final ConsoleUserPrincipal principal =
+                        new ConsoleUserPrincipal(username, "nimda", AuthorityUtils.createAuthorityList("ROLE_EMPLOYEEGROUP", "ROLE_ADMINGROUP"));
+        principal.setExpiryTime(-1L);
+        principal.setToken("");
 
-            /**
-             * It can't be POST because the CSRF is triggered.
-             */
-            HttpGet httpGet = new HttpGet(getRestBaseUrl() + "auth");
-
-            LOG.debug("Calling: " + getRestBaseUrl() + "auth");
-
-            httpGet.setHeader("X-Nemesis-Username", username);
-            httpGet.setHeader("X-Nemesis-Password", password);
-
-            HttpResponse response2 = httpclient.execute(httpGet);
-            HttpEntity entity2 = response2.getEntity();
-            final String response = EntityUtils.toString(entity2, Charset.defaultCharset());
-            LOG.info(response);
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
-            userData = mapper.readValue(response, UserData.class);
-            if (userData.getToken() == null) {
-                throw new BadCredentialsException("Invalid username/password");
-            }
-
-            final ConsoleUserPrincipal principal =
-                            new ConsoleUserPrincipal(userData.getUsername(), password, AuthorityUtils.createAuthorityList(userData.getAuthorities()));
-            principal.setExpiryTime(userData.getExpiryTime());
-            principal.setToken(userData.getToken());
-
-            return new UsernamePasswordAuthenticationToken(principal, password, principal.getAuthorities());
-        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException | IOException e) {
-            LOG.error(e.getMessage(), e);
-            throw new InternalAuthenticationServiceException(e.getMessage());
-        }
+        return new UsernamePasswordAuthenticationToken(principal, password, principal.getAuthorities());
+        //        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException | IOException e) {
+        //            LOG.error(e.getMessage(), e);
+        //            throw new InternalAuthenticationServiceException(e.getMessage());
+        //        }
     }
 
     @Override
