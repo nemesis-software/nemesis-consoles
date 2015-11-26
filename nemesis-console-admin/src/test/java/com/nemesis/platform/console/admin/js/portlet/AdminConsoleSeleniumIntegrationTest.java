c6680561ef7ebb03633594466286c0881d4756de
Petar Tahchiev <paranoiabla@gmail.com>
2015-11-26 09:34:35 +0200

    Add logging

--- nemesis-console-admin/src/test/java/com/nemesis/platform/console/admin/js/portlet/AdminConsoleSeleniumIntegrationTest.java
+++ nemesis-console-admin/src/test/java/com/nemesis/platform/console/admin/js/portlet/AdminConsoleSeleniumIntegrationTest.java
@@ -123,6 +123,7 @@ public class AdminConsoleSeleniumIntegrationTest extends AbstractCommonConsoleSe
         // find items by key & test their size
         getWebDriver().findElementByCssSelector("input[id^='system-properties-filter-input']").sendKeys("flyway.enabled");
         Thread.sleep(500);
+        LOG.info(getWebDriver().findElementsByCssSelector("div#system-properties-grid-body table.x-grid-item").size());
         assertTrue(getWebDriver().findElementsByCssSelector("div#system-properties-grid-body table.x-grid-item").size() > 1);
 
         // remove filter & assure that size of items shown is the same as before
