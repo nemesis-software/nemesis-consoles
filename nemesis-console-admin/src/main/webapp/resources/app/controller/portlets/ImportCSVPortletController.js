8a402eff2ebc10f32a60a1aeb1cfdc38781ccd7b
Nayden Gochev <gochev@gmail.com>
2016-06-01 17:37:00 +0300

    making the submitting of file to be manually with a workaround, because of no possibility with form submit to pass custom headers.

--- nemesis-console-admin/src/main/webapp/resources/app/controller/portlets/ImportCSVPortletController.js
+++ nemesis-console-admin/src/main/webapp/resources/app/controller/portlets/ImportCSVPortletController.js
@@ -73,19 +73,54 @@ Ext.define('AdminConsole.controller.portlets.ImportCSVPortletController', {
 				}
 			});
 		} else {
-			var form = Ext.getCmp('csv-import-form');
-			if (form.isValid()) {
-				form.submit({
-					headers: {
-						'X-Nemesis-Token': Ext.get('token').dom.getAttribute('value')
-					},
-					url: Ext.get('rest-base-url').dom.getAttribute('url') + 'platform/content/file-import',
-					waitMsg: 'Importing...',
-					success: function(fp, o) {
-						msg('Success', 'Processed file "' + o.result.file + '" on the server');
-					}
-				});
-			}
+			var csvFile = Ext.getCmp('csvFile');
+			var formData = new FormData();
+			// can't get the file path directly from the component due to browser security that does not allow for
+			// javascript to access the local file system directly Browser will return a "fakePath"
+			// reference to the actual local file, so getValue() will not work. To work around this, we have to access
+			// the dom file directly from the input element!!!
+			formData.append('file', csvFile.getEl().down('input[type=file]').dom.files[0]);
+			var req = new XMLHttpRequest();
+			req.open("POST", Ext.get('rest-base-url').dom.getAttribute('url') + 'platform/content/file-import', true);
+			// set headers and mime-type appropriately
+			req.setRequestHeader('X-Nemesis-Token', Ext.get('token').dom.getAttribute('value'));
+			req.onload = function() {
+				Ext.getCmp('csv-import-form').up().getEl().unmask();
+			};
+			req.onreadystatechange = function() {
+				if (req.readyState == 4 && req.status >= 400) {
+					req.hasError = true;
+					Ext.MessageBox.show({
+						title: 'Error',
+						msg: 'There was problem uploading the file.',
+						buttons: Ext.MessageBox.OK,
+						icon: Ext.MessageBox.ERROR
+					});
+				} else if(req.readyState == 4 && req.status == 200) {
+					Ext.toast({
+						html: 'Import started',
+						closable: false,
+						align: 't',
+						slideInDuration: 400,
+						minWidth: 400
+					});
+				}
+			};
+			Ext.getCmp('csv-import-form').up().getEl().mask("Uploading file ...");
+			req.send(formData);
+			// var form = Ext.getCmp('csv-import-form');
+			// if (form.isValid()) {
+			// 	form.submit({
+			// 		headers: {
+			// 			'X-Nemesis-Token': Ext.get('token').dom.getAttribute('value')
+			// 		},
+			// 		url: Ext.get('rest-base-url').dom.getAttribute('url') + 'platform/content/file-import',
+			// 		waitMsg: 'Importing...',
+			// 		success: function(fp, o) {
+			// 			msg('Success', 'Processed file "' + o.result.file + '" on the server');
+			// 		}
+			// 	});
+			// }
 		}
 	}
 
